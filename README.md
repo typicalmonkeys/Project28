819995ce8a855d82506144dfc6c77bdf1ac6dd63
typicalmonkeys <76875655+typicalmonkeys@users.noreply.github.com>
2021-05-08 20:24:22 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Project28
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
---
+++ Slingshot.js
@@ -0,0 +1,29 @@
+class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = pointB
+        this.Slingshot = Constraint.create(options);
+        World.add(world, this.Slingshot);
+    }
+    attach(body){
+        this.chain.bodyA = body;
+    }
+    fly(){
+        this.Slingshot.bodyA = null
+    }
+
+    display(){
+        if(this.Slingshot.bodyA){
+            var pointA = this.Slingshot.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+   
+}
